38ee86b2b0fe796e46b63a0ddb2626b44ebe7394
zxhom26 <125313839+zxhom26@users.noreply.github.com>
2026-03-13 15:42:07 -0500

    fix multiprocessing

--- ParallelORCSystemStudy.py
+++ ParallelORCSystemStudy.py
@@ -21,12 +21,13 @@ os.makedirs(output_folder, exist_ok=True)
 # read wells CSV
 base_path = Path(__file__).parent
 csv_path = base_path / data_file
-df_wells = pd.read_csv(csv_path, header=0, index_col=0)
+df_wells = pd.read_csv(csv_path, header=0, index_col=0).reset_index()
 
 def simulate_well(row):
     """Simulate a single well and return results."""
+
     try:
-        print(f"Simulating Well {row.Index}...")
+        print(f"Simulating Well {row['index']}...")
 
         params = SimulationParameters(working_fluid='water',
                                         orc_fluid='R245fa',
@@ -38,32 +39,32 @@ def simulate_well(row):
                                         rho_rock=2550.,)
 
         # assign well-specific parameters
-        if pd.isna(row.depth):
+        if pd.isna(row['depth']):
             raise ValueError("Depth is NaN, cannot simulate well") # fatal error
-        params.depth = row.depth
+        params.depth = row['depth']
 
-        if pd.notna(row.harrison_gradient):
-            params.dT_dz = row.harrison_gradient / 1000. # convert from K/km to K/m
+        if pd.notna(row['harrison_gradient']):
+            params.dT_dz = row['harrison_gradient'] / 1000. # convert from K/km to K/m
         else:
-            print(f"[Warning]: Harrison gradient is NaN for well {row.Index}, using default value of 35 K/km")
+            print(f"[Warning]: Harrison gradient is NaN for well {row['index']}, using default value of 35 K/km")
             params.dT_dz = 0.035
 
-        if pd.notna(row.surface_temp):
-            params.T_surface_rock = row.surface_temp
+        if pd.notna(row['surface_temp']):
+            params.T_surface_rock = row['surface_temp']
         else: 
-            print(f"[Warning]: Surface temperature is NaN for well {row.Index}, using default value of 15 C")
+            print(f"[Warning]: Surface temperature is NaN for well {row['index']}, using default value of 15 C")
             params.T_surface_rock = 15
         
-        if pd.notna(row.depth):
-            params.reservoir_thickness = row.depth # Assuming reservoir thickness is equal to depth!!!!
+        if pd.notna(row['depth']):
+            params.reservoir_thickness = row['depth'] # Assuming reservoir thickness is equal to depth!!!!
         else:
-            print(f"[Warning]: Depth is NaN for well {row.Index}, using default reservoir thickness of 100 m")
+            print(f"[Warning]: Depth is NaN for well {row['index']}, using default reservoir thickness of 100 m")
             params.reservoir_thickness = 100.
 
-        if pd.notna(row.k):
-            params.k_rock = row.k 
+        if pd.notna(row['k']):
+            params.k_rock = row['k']
         else:
-            print(f"[Warning]: Permeability is NaN for well {row.Index}, using default value of 2.08 mD")
+            print(f"[Warning]: Permeability is NaN for well {row['index']}, using default value of 2.08 mD")
             params.k_rock = 2.08
 
         # simulate system
@@ -78,23 +79,31 @@ def simulate_well(row):
         error_str = ''
 
     except Exception as e:
-        print(f"Error caught for well {row.Index}: {e}")
+        print(f"Error caught for well {row['index']}: {e}")
+        # Default input params and output results in case of error
+        params.dT_dz = 0.0
         lcoe_b = lcoe_g = power = optMdot = 0.
+
         error_str = str(e).replace("\n", "").replace(",", " - ")
 
-    return row.Index, row.depth, row.latitude, row.longitude, params.dT_dz, row.k, optMdot, lcoe_b, lcoe_g, power, error_str
+    return row['index'], row['depth'], row['latitude'], row['longitude'], row['bhtcorrected_temp'], params.dT_dz, row['k'], optMdot, lcoe_b, lcoe_g, power, error_str
 
-# run simulations in parallel
-results = []
-workers = os.cpu_count() - 1
-with ProcessPoolExecutor(max_workers=workers) as executor: 
-    futures = {executor.submit(simulate_well, row): row.Index for row in df_wells.itertuples()}
-    for future in as_completed(futures):
-        results.append(future.result())
+def main():
+    # run simulations in parallel
+    results = []
+    workers = max(1, os.cpu_count() - 1)
+    with ProcessPoolExecutor(max_workers=workers) as executor: 
+        futures = [executor.submit(simulate_well, row) for row in df_wells.to_dict(orient='records')]
+        for future in as_completed(futures):
+            results.append(future.result())
 
-# write results to CSV
-output_file_path = os.path.join(output_folder, output_file)
-with open(output_file_path, 'w') as output_file:
-    output_file.write("well,depth,latitude,longitude,thermal_gradient_K_m,k_mD,optMdot,lcoe_b,lcoe_g,power,error\n")
-    for res in results:
-        output_file.write(','.join([str(i) for i in res]) + "\n")
+    # write results to CSV
+    output_file_path = os.path.join(output_folder, output_file)
+    with open(output_file_path, 'w') as output_file:
+        output_file.write("well,depth,latitude,longitude,thermal_gradient_K_m,k_mD,optMdot,lcoe_b,lcoe_g,power,error\n")
+        for res in results:
+            output_file.write(','.join([str(i) for i in res]) + "\n")
+
+
+if __name__ == "__main__":
+    main()
